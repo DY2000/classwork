7430ac168bcf50393e6bfd63059fffeb75be89ee
yingz <yingz@LAPTOP-APEKNB6S.fios-router.home>
2017-09-27 00:08:23 -0400

    9/26/17

--- YangD/src/attendance/Student.java
+++ YangD/src/attendance/Student.java
@@ -1,35 +1,59 @@
 package attendance;
 
-public class Student implements Attendee{
-	private string first;
-	private string last;
-	private boolean present = false;
+public class Student implements Attendee {
+	private String first;
+	private String last;
+	private String absent;
+	private boolean present;
+	
 
-public Student(string firstName,string lastName){
+public Student(String firstName,String lastName){
 	this.first = firstName;
 	this.last = lastName;
 	
 }
-
-public boolean isPresent() {
-	
-			
-}
 public void setPresent(boolean present) {
 	this.present = present;
 }
-public string getFirstName(){
+public boolean isPresent() {
+	return this.present;
+			
+}
+
+public String getFirstName(){
 	return "Devin";
 }
-public string getLastName(){
+public String getLastName(){
 	return "Yang";
 }
 public boolean mathces(String first, String last) {
-	if() {
+	if(this.first.compareTo(first) == 0 && this.last.compareTo(last) == 0) {
 		return true;
 	}
 	else
 		return false;
+}
+public boolean matches(String first) {
+	if(this.first.compareTo(first) == 0) {
+		return true;
+	}
+	else
+		return false;
+	
+}
+public String getReportString() {
+	
+	while(this.first.length() < 20)
+{
+	this.first += "";
+}
+	this.last = this.first + this.last;
+	while (this.last.length() < 40)
+	{
+		this.last += "";
+	}
+		return this.absent = this.last + "Absent";
+	
 }
 }
 
