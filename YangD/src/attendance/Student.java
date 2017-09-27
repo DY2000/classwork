885fce75ba81be4f700a608e5d1b8ec358a581c0
yingz <yingz@LAPTOP-APEKNB6S.fios-router.home>
2017-09-26 20:51:13 -0400

    ??

--- YangD/src/attendance/Student.java
+++ YangD/src/attendance/Student.java
@@ -1,5 +1,35 @@
 package attendance;
 
-public class Student {
+public class Student implements Attendee{
+	private string first;
+	private string last;
+	private boolean present = false;
 
+public Student(string firstName,string lastName){
+	this.first = firstName;
+	this.last = lastName;
+	
 }
+
+public boolean isPresent() {
+	
+			
+}
+public void setPresent(boolean present) {
+	this.present = present;
+}
+public string getFirstName(){
+	return "Devin";
+}
+public string getLastName(){
+	return "Yang";
+}
+public boolean mathces(String first, String last) {
+	if() {
+		return true;
+	}
+	else
+		return false;
+}
+}
+
