a1a2a350eb8783939cdaeac4dadb5faae646e182
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2017-09-29 15:06:26 -0400

    cnianos

--- YangD/src/chatbot/Chatbot.java
+++ YangD/src/chatbot/Chatbot.java
@@ -1,9 +1,35 @@
 package chatbot;
 
 public class Chatbot {
+	private String username;
+	private boolean chatting;
+	private Topic dy;
 
 	public Chatbot() {
-		// TODO Auto-generated constructor stub
+		dy = new Chatbotdy();
+		username = "Unknown User";
+		chatting = true;
 	}
+
+	public String getUsername() {
+		return username;
+	}
+	
+	public void startChatting() {
+		ChatbotMain.print("Hi! I am an intelligent machine that can respond to your inputs. Tell me your name.");
+		username = ChatbotMain.getInput();
+		while(chatting) {
+			ChatbotMain.print("What would you like to talk about?");
+			String response = ChatbotMain.getInput();
+			if(dy.isTriggered(response)) {
+				chatting = false;//exits the while loop. IMPORTANT you get graded for this!
+				dy.talk(response);
+				
+			}else {
+				ChatbotMain.print("I'm sorry. I don't understand. I never said I was perfect.");
+			}
+		}
+	}
+
 
 }
--- YangD/src/chatbot/Topic.java
+++ YangD/src/chatbot/Topic.java
@@ -1,5 +1,10 @@
 package chatbot;
 
 public interface Topic {
+	
+	void talk(String response);
+
+	boolean isTriggered(String response);
+
 
 }
