4a00d7ec442685e9187f32a0c4cdec26d1d0b200
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2017-10-17 15:30:21 -0400

    kk

--- YangD/src/arrays/ArraysMain.java
+++ YangD/src/arrays/ArraysMain.java
@@ -9,6 +9,72 @@ public class ArraysMain {
 	private String[] values;
 	
 	public ArraysMain() {
+		tuesdayMethods();
+	}
+	
+	private void tuesdayMethods() {
+		int[] orderTest = {1,2,3,4,5,1,6,7,8,9,10};
+//		cycleThrough(orderTest, 5);
+//		System.out.println(Arrays.toString(orderTest));
+		System.out.println(longestConsecutiveSequence(orderTest));
+	}
+
+	private void cycleThrough(int[] arr, int n) {
+	for(int i = 0; i < arr.length-1; i++) {
+			frontToBack(arr);
+	}
+	}
+	private int longestConsecutiveSequence(int[] arr) {
+		int maxLength = 1;
+		int currentCount = 1;
+		for(int i = 0; i < arr.length; i++) {
+			while(i + currentCount < arr.length &&
+					isConsecutive(arr, i, i+currentCount)) {
+			currentCount++;
+		}
+			if(currentCount > maxLength ) {
+				maxLength = currentCount;
+				
+			}
+			i = i + currentCount-1;
+			currentCount = 1;
+		}
+		return maxLength;
+	}	
+	/**
+	 * returns true if all of the elements from start to end are increasing by 1
+	 * example:
+	 * iC({1,7,3,6,7,8,12},3,5) -> true;
+	 * @param arr
+	 * @param start
+	 * @param end
+	 * @return
+	 */
+	private boolean isConsecutive(int[] arr, int start, int end) {
+		for(int i = start; i < end; i++) {
+			if(arr[i] + 1 != arr[i+1]) {
+				return false;
+			}
+			return true;
+			
+		}
+		
+	}
+
+	private void frontToBack(int[] arr) {
+		for(int i = 0; i < arr.length-1; i++) {
+			swap(arr,i,i+1);
+		}
+	}
+	private void warmUpMethods() {
+		int[] orderTest = {1,2,3,4,5,6,7,8,9,10};
+		reverseOrder(orderTest);
+		System.out.println(Arrays.toString(orderTest));
+		System.out.println(Arrays.toString(subArray(orderTest,3,4)));
+	}
+
+	
+	public cardEngine() {
 		suits = new String[4];
 		suits[0] = "Clubs";
 		suits[1] = "Hearts";
@@ -24,6 +90,7 @@ public class ArraysMain {
 		values[3] = "Jack";
 		printDeck();
 		
+		//reverseOrder(testArray);
 		//testArray = new int[50];
 		//populate1ToN(testArray);
 		//shuffle(testArray);
@@ -31,6 +98,15 @@ public class ArraysMain {
 		//populate(testArray);
 		//countOccurences(testArray,2,12);
 		//System.out.println(Arrays.toString(testArray));
+	}
+	
+	private int[] subArray(int[] arr, int psn, int length) {
+		int[] sub = new int[length];
+		for(int i = 0; i < length; i++) {
+			sub[i] = arr[i+psn];
+		}
+		return sub;
+		
 	}
 	private String[] printDeck() {
 		String[] deck = new String[52];
@@ -55,6 +131,10 @@ public class ArraysMain {
 		arr[i] = arr[j];
 		arr[j] = placeholder;
 
+	}
+	private void reverseOrder(int[] arr) {
+		for(int i = 0; i < arr.length/2; i++) 
+			swap(arr,i,arr.length-1-i);
 	}
 	private void populate1ToN(int[] arr) {
 		for(int i = 0; i < arr.length; i++) {
