2d04110a4a1b57dc5b1a87417e352ebe20667e05
yingz <yingz@LAPTOP-APEKNB6S.fios-router.home>
2018-04-24 00:37:35 -0400

    was

---
+++ YangD/src/algoritms/NEW.java
@@ -0,0 +1,26 @@
+package algoritms;
+
+public class NEW {
+	
+
+	
+	public static String repeat(int num)
+	{
+		String bounce =  " "+ (num / 7) ;
+	
+		if(num % 5  > 2 || num < 50){
+			return bounce + repeat(num / 2);
+			
+		}
+		return bounce;
+
+		
+	}
+	
+
+	public static void main(String [] args) {
+		System.out.print(repeat(49));
+	}
+}
+
+
